f5eb927fc1a63cf53a42e51189647cb63714f5c7
Alex Leiba <alexandru@enovatika.com>
2024-12-18 09:34:04 +0200

    changes

--- frontend/store/useAuthStore.ts
+++ frontend/store/useAuthStore.ts
@@ -52,7 +52,8 @@ type Store = {
   onlineUsers: string[];
 };
 
-const BACKEND_BASE_URL = 'http://localhost:5001/';
+// const BACKEND_BASE_URL = 'http://localhost:5001/';
+const BACKEND_BASE_URL = 'https://chat-me-full-stack-app.onrender.com/';
 
 export type FormType = Zod.infer<typeof SignupSchema>;
 
