171440fb4e8854634d5b725a5e4b6da7f3968ace
Alex Leiba <alexandru@enovatika.com>
2024-12-19 13:14:39 +0200

    axios fix

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import { cookies } from 'next/headers'; // For server-side cookie handling
 
 const axiosInstance = axios.create({
-  baseURL: 'https://chat-me-full-stack-app.onrender.com', // production
+  baseURL: 'https://chat-me-full-stack-app.onrender.com/api', // production
   // baseURL: 'http://localhost:5001/api', //local
   withCredentials: true, // Ensures cookies are included in requests
 });
