f60781b10ea1f9f1194433b05007b13160711741
Alex Leiba <alexandru@enovatika.com>
2024-12-19 17:21:20 +0200

    utils

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -18,5 +18,6 @@ export const generateToken = (userPayload, res) => {
     // httpOnly: true, //preventing cookie from being accessed by client side js, prevents XSS attacks (cross site scripting attacks)
     // sameSite: 'strict', //CSRF protection, request forgery attacks
     // secure: process.env.NODE_ENV !== 'development', // cookie only sent over HTTPS not HTTP
+    sameSite: 'lax',
   });
 };
