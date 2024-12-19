e98b1e05ced488aa552dd5ddba67fabcc357f72e
Alex Leiba <alexandru@enovatika.com>
2024-12-19 13:28:43 +0200

    change http only

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,7 +15,7 @@ export const generateToken = (userPayload, res) => {
   // Create JWT token //
   res.cookie('chat-me-token', token, {
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-    httpOnly: true, //preventing cookie from being accessed by client side js, prevents XSS attacks (cross site scripting attacks)
+    // httpOnly: true, //preventing cookie from being accessed by client side js, prevents XSS attacks (cross site scripting attacks)
     sameSite: 'strict', //CSRF protection, request forgery attacks
     secure: process.env.NODE_ENV !== 'development', // cookie only sent over HTTPS not HTTP
   });
